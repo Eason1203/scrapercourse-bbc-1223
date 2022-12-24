847255d0b72f8fdec57c66fd008fcc2e5e659891
Eason12030105 <eason12030105@gmail.com>
2022-12-24 08:35:10 +0800

    3.5批次爬爬取多分頁的網頁內容

--- scraper.py
+++ scraper.py
@@ -1,32 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
+for page in range(1,4):
+    response = requests.get(f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt?page={page}')
+    #print (response.text) 取得bbc 財金網址
 
-response = requests.get('https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt')
-#print (response.text) 取得bbc 財金網址
+    soup = BeautifulSoup(response.text, 'lxml') 
+    titles = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
+    #print(titles) 透過Beautifulsoup取得網頁內容 透過Find all 找出所有標籤下的值
 
-soup = BeautifulSoup(response.text, 'lxml') 
-titles = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
-#print(titles) 透過Beautifulsoup取得網頁內容 透過Find all 找出所有標籤下的值
+    title_list=[]
+    for title in titles:
+        title_list.append(title.getText())
 
-title_list=[]
-for title in titles:
-    title_list.append(title.getText())
+    #print(title_list) 將所有網址的新聞Title放到list 裡面
 
-#print(title_list) 將所有網址的新聞Title放到list 裡面
+    urls = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
 
-urls = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
 
-tag_list=[]
-for url in urls:
-    #print(url.get('href'))
-    #print(title_list) 將所有網址的新聞網址找出來
-    sub_response = requests.get(url.get('href'))
-    sub_soup = BeautifulSoup(sub_response.text, 'lxml')
-    tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e2o6ii40'})
-    for tag in tags:
-        tag_list.append(tag.find('a').getText())
-         # 將所有網址新聞的附屬Title列出來
-print(tag_list)
+    tag_list=[]
+    for url in urls:
+        #print(url.get('href'))
+        #print(title_list) 將所有網址的新聞網址找出來
+        sub_response = requests.get(url.get('href'))
+        sub_soup = BeautifulSoup(sub_response.text, 'lxml')
+        tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e2o6ii40'})
+        for tag in tags:
+            tag_list.append(tag.find('a').getText())
+            # 將所有網址新聞的附屬Title列出來
+    
+    print(f"第{page}頁")
+    print(title_list)
+    print(tag_list)
 
        
